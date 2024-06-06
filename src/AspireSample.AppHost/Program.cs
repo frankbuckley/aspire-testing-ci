609d278006f61976202b92a6b879eee2d278a60b
Frank Buckley <frank@frankbuckley.com>
2024-06-06 10:58:51 +0100

    Reference dapr state store

--- src/AspireSample.AppHost/Program.cs
+++ src/AspireSample.AppHost/Program.cs
@@ -2,15 +2,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddDapr();
 
+var state = builder.AddDaprStateStore("state");
+
 var pubsub = builder.AddDaprPubSub("pubsub");
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
     .WithDaprSidecar()
+    .WithReference(state)
     .WithReference(pubsub);
 
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
     .WithDaprSidecar()
     .WithExternalHttpEndpoints()
+    .WithReference(state)
     .WithReference(apiService);
 
 builder.Build().Run();
