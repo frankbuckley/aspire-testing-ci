a595a82f9a3d20298a67998fc803717d33bd78a8
Frank Buckley <frank@frankbuckley.com>
2024-06-06 10:55:24 +0100

    Reference sidecars

--- src/AspireSample.AppHost/Program.cs
+++ src/AspireSample.AppHost/Program.cs
@@ -5,9 +5,11 @@ builder.AddDapr();
 var pubsub = builder.AddDaprPubSub("pubsub");
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
+    .WithDaprSidecar()
     .WithReference(pubsub);
 
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+    .WithDaprSidecar()
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
